b8482ce382e1e2669c7df3e8e4cdde4db6aac040
Jose Leonardo Quiñones <desarrollojlq@outlook.es>
2025-07-03 18:17:54 -0500

    Add initial project structure with task model, create component, and format due date pipe

---
+++ src/app/core/pipes/format-due-date-pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FormatDueDatePipe } from './format-due-date-pipe';
+
+describe('FormatDueDatePipe', () => {
+  it('create an instance', () => {
+    const pipe = new FormatDueDatePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
