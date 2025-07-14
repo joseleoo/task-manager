7109e104a908a13e291a7e780b72e47e4ea724d2
Jose Leonardo Quiñones <desarrollojlq@outlook.es>
2025-07-13 20:58:18 -0500

    feat: Enhance Parent and Child components with message handling and clear functionality

--- src/app/lessons/input-signals/child/child.ts
+++ src/app/lessons/input-signals/child/child.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -8,4 +8,13 @@ import { Component, Input } from '@angular/core';
 })
 export class Child {
   @Input() message: string = '';
+  @Output() sendMessage = new EventEmitter<string>();
+
+  send() {
+    this.sendMessage.emit('Hello from Child!');
+  }
+
+  clearChild() {
+    this.sendMessage.emit('');
+  }
 }
